59a31db6fc0d0a5cd2ee17d0bdf21abe34e3d6b7
liudongsheng <1642095123@qq.com>
2026-01-15 22:32:41 +0800

    细节处理

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,45 @@
 import { fileURLToPath, URL } from 'node:url'
-import path from 'path'
 
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-const def = {
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    createSvgIconsPlugin({
-      // 指定需要缓存的图标文件夹
-      iconDirs: [path.resolve('src/assets/icons')],
-      // 指定symbolId格式
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+const def = (mode) => {
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      vueDevTools(),
+      mode === 'analyze' &&
+        visualizer({
+          open: true,
+          filename: 'dist/stats.html',
+          gzipSize: true,
+          brotliSize: true,
+        }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          if (/\.css$/.test(assetInfo.name)) {
-            return 'css/[name]-[hash][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'js/[name]-[hash].js',
+          entryFileNames: 'js/[name]-[hash].js',
+          assetFileNames: (assetInfo) => {
+            if (/\.css$/.test(assetInfo.name)) {
+              return 'css/[name]-[hash][extname]'
+            }
+            return 'assets/[name]-[hash][extname]'
+          },
         },
       },
     },
-  },
+  }
 }
 
 const dev = {
@@ -56,14 +58,14 @@ const dev = {
 }
 
 const prod = {
-  base: '/admin/',
+  base: '/vue3-admin/',
 }
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 
   return {
-    ...def,
+    ...def(mode, env),
     ...(env.VITE_APP_ENV === 'production' ? prod : dev),
   }
 })
